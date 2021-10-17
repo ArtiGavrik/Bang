d17ec4aaecdf9aa6dae8869fb3034ab77cd89bad
unknown <art20021409@gmail.com>
2021-10-17 19:27:33 +0300

    lab_2

---
+++ Project/Gavrilov/Kvad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gavrilov.Gavrilov
+{
+    class Kvad:Lin
+    {
+        protected double Discriminant(double a, double b, double c)
+        {
+            return b * b - (4 * a * c);
+        }
+        protected double[] Func(int a, int b, int c)
+        {
+            if (a == 0)
+            {
+                return Func(b, c);
+            }
+            double disc = Discriminant(a, b, c);
+            if (disc == 0)
+            {
+                return x = new double[] { -b / (2 * a) };
+            }
+            if (disc > 0)
+            {
+                disc = Math.Sqrt(disc);
+                return x = new double[]
+                {
+                    (-b + disc) / (2 * a),
+                    (-b - disc) / (2 * a)
+                };
+            }
+            throw new Exception("Корней нет");
+        }
+    }
+}
---
+++ Project/Gavrilov/Lin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gavrilov.Gavrilov
+{
+    class Lin
+    {
+        protected double[] x;
+        public double[] Func(double a, double b)
+        {
+            if (a == 0)
+            {
+                throw new Exception("Нет корней");
+            }
+            return x = new double[] { (-b / a) };
+        }
+    }
+}
